a502ee87e36a9a2d96035843d1d3f9da6d46df6a
Brett Davidson <brett@davidson.gen.nz>
2021-07-01 16:14:07 +1200

    Second commit

--- README.md
+++ README.md
@@ -3,3 +3,9 @@ GitHub repository for DSYS601 classwork
 
 This repo is for student experimentation with git so will most likely be unstable, inaccurate and plain wonky at times.
 That's part of their learning!
+
+**************
+git is great. git is opensource. git is free. But git is not a git.
+You can use git on it's own or use it with GitHub.
+git has many features. git is powerful.
+**************
--- prelim_setup.sh
+++ prelim_setup.sh
@@ -8,6 +8,7 @@ sed -ibak 's/#force_color_prompt=yes/force_color_prompt=yes/' ~/.bashrc
 echo "alias ug='git add . && git commit -a'" > ~/.bash_aliases
 echo "alias ai='apt install'" >> ~/.bash_aliases
 echo "alias au='apt update && apt upgrade -y'" >> ~/.bash_aliases
+echo "alias ibl-'sed -E '/(^\s*#)|(^\s*$)/d'" >> ~/.bash_aliases
 # Re-Read the profile settings to pick up the new changes
 . ~/.bashrc
 # Exit the shell as you'll need to start a new shell to pick up the settings
